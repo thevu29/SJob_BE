17115590d2139147311e34d6c5b1b2835e3655eb
thevu29 <vnguyen132az@gmail.com>
2025-03-06 10:01:43 +0700

    clean import

--- user-service/src/main/java/org/example/userservice/exception/GlobalExceptionHandler.java
+++ user-service/src/main/java/org/example/userservice/exception/GlobalExceptionHandler.java
@@ -2,8 +2,6 @@ package org.example.userservice.exception;
 
 import lombok.extern.slf4j.Slf4j;
 import org.example.userservice.dto.ApiResponse;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
@@ -12,7 +10,6 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
 
