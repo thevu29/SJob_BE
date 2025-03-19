46f1fc36c456f80aa2b8f4459cd89ada0417b7f6
HoangVuong1910 <huyhoang191072@gmail.com>
2025-03-19 18:58:09 +0700

    chore: downgrade jdk version for all service

---
+++ job-service/src/main/java/com/example/jobservice/controller/JobController.java
@@ -0,0 +1,16 @@
+package com.example.jobservice.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/jobs")
+@RequiredArgsConstructor
+public class JobController {
+    @GetMapping
+    public String getJobs() {
+        return "Jobs";
+    }
+}
