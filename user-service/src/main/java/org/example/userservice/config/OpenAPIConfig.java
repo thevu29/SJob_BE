e0411890ebaa8f8d9884e3e9afc168b1a40b0d17
thevu29 <vnguyen132az@gmail.com>
2025-03-31 21:10:32 +0700

    refactor: fix base api url in swagger

--- user-service/src/main/java/org/example/userservice/config/OpenAPIConfig.java
+++ user-service/src/main/java/org/example/userservice/config/OpenAPIConfig.java
@@ -2,8 +2,11 @@ package org.example.userservice.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class OpenAPIConfig {
@@ -14,6 +17,9 @@ public class OpenAPIConfig {
                         .title("User Service API")
                         .version("1.0")
                         .description("API documentation for the User Service")
-                );
+                )
+                .servers(List.of(
+                        new Server().url("http://localhost:8080").description("Local server")
+                ));
     }
 }
