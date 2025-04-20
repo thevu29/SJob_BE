8ea6e8bf6145bf7f3f8cb03a0fc8f41a766f7e4a
thevu29 <vnguyen132az@gmail.com>
2025-04-20 15:55:51 +0700

    refactor: remove log

--- email-service/src/main/java/org/example/emailservice/kafka/EmailListener.java
+++ email-service/src/main/java/org/example/emailservice/kafka/EmailListener.java
@@ -2,7 +2,6 @@ package org.example.emailservice.kafka;
 
 import jakarta.mail.MessagingException;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.common.dto.Email.EmailMessageDTO;
 import org.example.emailservice.services.EmailService;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -10,13 +9,11 @@ import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
-@Slf4j
 public class EmailListener {
     private final EmailService emailService;
 
     @KafkaListener(topics = "send-email", groupId = "email-service")
     public void listen(EmailMessageDTO emailMessage) throws MessagingException {
-        log.info("Received message for email: {}", emailMessage.getTo());
         emailService.sendEmail(emailMessage);
     }
 }
