b84944f6d7365f198458fa1d4e075831510e37a8
thevu29 <vnguyen132az@gmail.com>
2025-04-20 15:07:31 +0700

    refactor: move migration/ and seed/ to database/

--- user-service/src/main/java/org/example/userservice/database/migration/RemoveDeletedAtFieldMigration.java
+++ user-service/src/main/java/org/example/userservice/database/migration/RemoveDeletedAtFieldMigration.java
@@ -1,4 +1,4 @@
-package org.example.userservice.migration;
+package org.example.userservice.database.migration;
 
 import io.mongock.api.annotations.ChangeUnit;
 import io.mongock.api.annotations.Execution;
@@ -8,19 +8,19 @@ import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.data.mongodb.core.query.Update;
 
-@ChangeUnit(id = "add-deleted-at-field", order = "001")
-public class AddDeletedAtFieldMigration {
+@ChangeUnit(id = "remove-deleted-at-field", order = "003")
+public class RemoveDeletedAtFieldMigration {
     @Execution
     public void changeSet(MongoTemplate mongoTemplate) {
-        Query query = new Query(Criteria.where("deletedAt").exists(false));
-        Update update = new Update().set("deletedAt", null);
+        Query query = new Query(Criteria.where("deletedAt").exists(true));
+        Update update = new Update().unset("deletedAt");
         mongoTemplate.updateMulti(query, update, "users");
     }
 
     @RollbackExecution
     public void rollback(MongoTemplate mongoTemplate) {
-        Query query = new Query(Criteria.where("deletedAt").exists(true));
-        Update update = new Update().unset("deletedAt");
+        Query query = new Query(Criteria.where("deletedAt").exists(false));
+        Update update = new Update().set("deletedAt", null);
         mongoTemplate.updateMulti(query, update, "users");
     }
 }
