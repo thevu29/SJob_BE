56a07b20b7869c6e62741acc9e50433de5c5ef66
Acer <vnguyen132az@gmail.com>
2025-05-22 14:51:36 +0700

    refactor: remove unnecessary imports

--- application-service/src/main/java/org/example/applicationservice/config/SecurityConfig.java
+++ application-service/src/main/java/org/example/applicationservice/config/SecurityConfig.java
@@ -2,14 +2,11 @@ package org.example.applicationservice.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
